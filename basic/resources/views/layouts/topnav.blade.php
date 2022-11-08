234d86ae36593136dc1fcc6f193cfc50ce5494dd
FasiulAshan <FasiulAshan1997@gmail.com>
2022-11-08 12:44:44 +0600

    5th commit

--- basic/resources/views/layouts/topnav.blade.php
+++
@@ -1,6 +0,0 @@
-<a class= "btn btn-primary" href="{{route('home')}}">Home</a>
-<a class= "btn btn-primary" href="{{route('profile')}}">Profile</a>
-<a class= "btn btn-primary" href="{{route('other')}}">Other</a>
-<a class= "btn btn-primary" href="{{route('studentlist')}}">Student List</a>
-<a class= "btn btn-primary" href="{{route('studentCreate')}}">Student Create</a>
-
